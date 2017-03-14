3f72dc5259643d206979cb91bcf0caf643cde8e2
Andrew W <www.andrewwang@gmail.com>
2017-03-14 01:30:41 -0500

    DUMBEST
    MISTAKE
    EVEr

--- src/altcontains.m
+++ src/altcontains.m
@@ -2,7 +2,7 @@ function inds = altcontains(cells, str)
     n = length(cells);
     inds = zeros(size(cells));
     for i = 1:n
-        if (~isempty(strfind(cells{i}, '.mat')))
+        if (~isempty(strfind(cells{i}, str)))
             inds(i) = true;
         else
             inds(i) = false;
